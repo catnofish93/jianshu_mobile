bcf704fc6b702938e42631adf6dfedef4b308abc
yuanwenhui <yuanwenhui93@163.com>
2021-09-02 14:04:33 +0800

    feat: 底部导航拦

---
+++ src/components/home/prefer.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+function SettingsScreen() {
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>prefer!</Text>
+      </View>
+  );
+}
+export default SettingsScreen
---
+++ src/page/home.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {View, useWindowDimensions} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
+import activity from '../components/home/activity';
+import prefer from '../components/home/prefer';
+import suggest from '../components/home/suggest';
+import topic from '../components/home/topic';
+
+const renderScene = SceneMap({
+  activity: activity,
+  prefer: prefer,
+  suggest: suggest,
+  topic: topic
+});
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'activity', title: '活动'},
+    {key: 'suggest', title: '推荐'},
+    {key: 'topic', title: '专题'},
+    {key: 'prefer', title: '优选'},
+  ]);
+
+  return (
+    <TabView
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{width: layout.width}}
+    />
+  );
+}
