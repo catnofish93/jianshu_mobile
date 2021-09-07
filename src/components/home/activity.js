8216600f9120aef5cc1341c17724907673d57ba7
yuanwenhui <yuanwenhui93@163.com>
2021-09-07 12:03:19 +0800

    feat: 首页开发

--- src/components/home/activity.js
+++ src/components/home/activity.js
@@ -11,7 +11,7 @@ import banner from '../../images/banner-s-daily-e6f6601abc495573ad37f2532468186f
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: '优质内容限时免费阅读，快去看看～',
+    title: '优质内容限时免费阅读，快去看看～12112',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
@@ -27,7 +27,7 @@ const DATA = [
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: '【反套路爱情】打破传统的爱情围城',
+    title: '【反套路爱情】打破传统的爱情围城111',
   },
 ];
 class SettingsScreen extends React.Component {
@@ -37,20 +37,6 @@ class SettingsScreen extends React.Component {
   render() {
     return (
       <View>
-        <ScrollView
-          horizontal={true}
-          style={styles.scroll}
-          showsHorizontalScrollIndicator={true}>
-          <Text style={styles.text}>综合</Text>
-          <Text style={styles.text}>最新</Text>
-          <Text style={styles.text}>读书</Text>
-          <Text style={styles.text}>文学</Text>
-          <Text style={styles.text}>职场</Text>
-          <Text style={styles.text}>诗词</Text>
-          <Text style={styles.text}>文学</Text>
-          <Text style={styles.text}>职场</Text>
-          <Text style={styles.text}>诗词</Text>
-        </ScrollView>
         <FlatList
             style={styles.list}
           data={DATA}
@@ -67,17 +53,8 @@ class SettingsScreen extends React.Component {
   }
 }
 const styles = StyleSheet.create({
-  scroll: {
-    backgroundColor: '#fff',
-    padding: 10,
-  },
-  text: {
-    padding: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    backgroundColor: '#eee',
-    marginRight: 15,
-    borderRadius: 20,
+  list: {
+    paddingBottom: 200
   },
   listItem: {
     backgroundColor: '#fff',
@@ -87,7 +64,8 @@ const styles = StyleSheet.create({
     paddingRight: 15
   },
   image: {
-    resizeMode: 'contain'
+    resizeMode: 'contain',
+    width: '100%'
   }
 });
 export default SettingsScreen;
--- src/components/home/suggest.js
+++ src/components/home/suggest.js
@@ -1,11 +1,56 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
-
+import {ScrollView, StyleSheet, Text, View, FlatList, Image} from 'react-native';
+const DATA = [1, 2, 3]
 function SettingsScreen() {
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>suggest!</Text>
+      <View>
+        <ScrollView
+            horizontal={true}
+            style={styles.scroll}
+            showsHorizontalScrollIndicator={true}>
+          <Text style={styles.text}>综合</Text>
+          <Text style={styles.text}>最新</Text>
+          <Text style={styles.text}>读书</Text>
+          <Text style={styles.text}>文学</Text>
+          <Text style={styles.text}>职场</Text>
+          <Text style={styles.text}>诗词</Text>
+          <Text style={styles.text}>文学</Text>
+          <Text style={styles.text}>职场</Text>
+          <Text style={styles.text}>诗词</Text>
+        </ScrollView>
+        <Text>11111</Text>
+        <FlatList>
+          data={DATA}
+          renderItem={item => (
+            <View>
+              <View>
+                <Image source={''} style={styles.authorImage}></Image>
+                <Text>阿园妈咪</Text>
+              </View>
+              <View style={styles.title}>
+                舒服的聊天方式
+              </View>
+              <View style={styles.content}>
+                今天碰到一个发小，她说她在厂里上夜班，不过马上就辞职了。上了也有5个月了吧，她说上夜班特别难熬。我特别支持她辞掉这个工作。我觉得吧！，女人不到万...
+              </View>
+            </View>
+          )}
+        </FlatList>
       </View>
   );
 }
+const styles = StyleSheet.create({
+  scroll: {
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+  text: {
+    padding: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: '#eee',
+    marginRight: 15,
+    borderRadius: 20,
+  }
+})
 export default SettingsScreen
